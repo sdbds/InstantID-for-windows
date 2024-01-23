433c8c241a8eac47ece6bf33c5b06cbcce796785
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2024-01-23 22:08:40 +0800

    init

---
+++ install_cn.ps1
@@ -0,0 +1,19 @@
+Set-Location $PSScriptRoot
+
+$Env:PIP_DISABLE_PIP_VERSION_CHECK = 1
+$Env:PIP_INDEX_URL = "https://mirror.baidu.com/pypi/simple"
+$Env:HF_ENDPOINT = "https://hf-mirror.com"
+
+if (!(Test-Path -Path "venv")) {
+    Write-Output  "创建python虚拟环境venv..."
+    python -m venv venv
+}
+.\venv\Scripts\activate
+
+Write-Output "安装依赖..."
+pip install -U -r gradio_demo/requirements-windows.txt
+
+huggingface-cli download --resume-download InstantX/InstantID --local-dir checkpoints
+
+Write-Output "安装完毕"
+Read-Host | Out-Null ;
